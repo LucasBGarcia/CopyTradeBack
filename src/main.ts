33fe928445020461c24a380fb117f6045f7020e3
LucasBGarcia <barbozagarcia@yahoo.com.br>
2024-05-15 09:47:51 -0300

    funcionando corretamente

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   const corsOptions: CorsOptions = {
+//     origin: ['https://bottrade-git-develop-lucasbgarcias-projects.vercel.app'], // Substitua pelo domínio do seu front-end na Vercel
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   };
+//   app.enableCors(corsOptions);
+
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   // Configurando CORS
   app.enableCors();
 

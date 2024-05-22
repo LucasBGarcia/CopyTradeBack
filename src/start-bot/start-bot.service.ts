9cef652d8adae55f7a34c1201919558691cfbcdb
LucasBGarcia <barbozagarcia@yahoo.com.br>
2024-05-22 10:13:59 -0300

    .

--- src/start-bot/start-bot.service.ts
+++ src/start-bot/start-bot.service.ts
@@ -5,8 +5,11 @@ import * as WebSocket from 'ws';
 export class StartBotService {
     async StartBot(listenKey: string, AtivaBot: boolean): Promise<any> {
         const BINANCE_WS_URL = 'wss://stream.binance.com:9443/ws'
-        console.log('LISTENKEY', listenKey, 'ATIVABOT', AtivaBot)
+        console.log('LISTENKEY', listenKey)
+        console.log('ATIVABOT', AtivaBot)
+
         if (listenKey && AtivaBot) {
+            console.log('ta caindo dentro do if')
             return new Promise((resolve, reject) => {
                 try {
                     const ws = new WebSocket(`${BINANCE_WS_URL}/${listenKey}`);
